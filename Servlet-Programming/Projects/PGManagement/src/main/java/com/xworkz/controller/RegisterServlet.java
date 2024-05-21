f07348689c811114376abce4c37b8797394fdd26
Sahil-123 <sahilnaikwadi123@gmail.com>
2024-05-22 03:48:40 +0530

    Logger slf4j api and logback provider is added. it provides proper logging.

--- Servlet-Programming/Projects/PGManagement/src/main/java/com/xworkz/controller/RegisterServlet.java
+++ Servlet-Programming/Projects/PGManagement/src/main/java/com/xworkz/controller/RegisterServlet.java
@@ -2,6 +2,8 @@ package com.xworkz.controller;
 
 import com.xworkz.service.RegisterService;
 import com.xworkz.service.RegisterServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +15,7 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/register",loadOnStartup = 1)
 public class RegisterServlet extends HttpServlet {
-
+    private static final Logger logger = LoggerFactory.getLogger(RegisterServlet.class);
     public RegisterServlet(){
         System.out.println(" RegisterServlet initialized successfully");
     }
@@ -21,6 +23,7 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        logger.info("Customer register is processing...");
         RegisterService registerService = new RegisterServiceImpl();
         registerService.register(req,resp);
     }
